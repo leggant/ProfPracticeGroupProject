d1eb97106c3e4fc9bf71c0c1983f6a9010493cb1
leggant <61127354+leggant@users.noreply.github.com>
2020-05-04 14:55:51 +1200

    Start Point Project Files

---
+++ PP_Final_Group_Project_2020/AI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+namespace PP_Final_Group_Project_2020
+{
+    class AI
+    {
+        static void Main()
+        {
+            Console.ReadLine();
+        }
+    }
+}
