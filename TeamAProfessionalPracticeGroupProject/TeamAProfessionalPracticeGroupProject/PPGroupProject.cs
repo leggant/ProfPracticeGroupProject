88964516958427170af1f7370b30dea3deb43df3
leggant <61127354+leggant@users.noreply.github.com>
2020-05-06 11:47:34 +1200

    Completed the script to add external text file content in to the program. These are loaded in to seperate arrays which can be referenced later.  The arrays may need to be changed in to multi dimensional arrays to account for the different categories of questions and answers.  Or these could be split in to seperate text files that are loaded in to seperate additional arrays .

--- TeamAProfessionalPracticeGroupProject/TeamAProfessionalPracticeGroupProject/PPGroupProject.cs
+++ TeamAProfessionalPracticeGroupProject/TeamAProfessionalPracticeGroupProject/PPGroupProject.cs
@@ -7,16 +7,51 @@ namespace TeamAProfessionalPracticeGroupProject
     {
         static void Main()
         {
+            // Variable Declarations
+            string[] questions = new string[10];
+            string[] answers = new string[10];
+            int userchoice;
+            string NameInput, temp = "";
+            string hashlines = "################################################################";
+            string welcomeText = "###########Welcome, This Program is Here To Help You!###########\nCovid-19 Lockdown has been tough, so we are here to check in.";
+            
+            //Start Program
+            // Welcome User & Ask for their name
+
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("################################################################");
-            Console.WriteLine("###########Welcome, This Program is Here To Help You!###########\nCovid-19 Lockdown has been tough, so we are here to check in.");
-            Console.WriteLine("################################################################");
+            Console.WriteLine(hashlines);
+            Console.WriteLine(welcomeText);
+            Console.WriteLine(hashlines);
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Please Enter your name :");
-            string NameInput = Console.ReadLine();
-            Console.Write($"Hi {NameInput}");
-            Console.WriteLine("On scale of 1 to 10 how you feeling today??");
-            int userchoice = Convert.ToInt32(Console.ReadLine());
+            NameInput = Console.ReadLine();
+            Console.Write($"Hi {NameInput}, on scale of 1 to 10 how you feeling today?? ");
+            userchoice = Convert.ToInt32(Console.ReadLine());
+
+            //Loading text string data in to an array here.
+            
+            StreamReader questionsTextFileReader = new StreamReader("QuestionStrings.txt");
+            StreamReader answersTextFileReader = new StreamReader("AnswerStrings.txt");
+            
+            while (temp != null)
+            {
+                for (int i = 0; i < questions.Length; i++)
+                {
+                    temp = questionsTextFileReader.ReadLine();
+                    questions[i] = temp;
+                }
+                for (int i = 0; i < answers.Length; i++)
+                {
+                    temp = answersTextFileReader.ReadLine();
+                    answers[i] = temp;
+                }
+            }
+            questionsTextFileReader.Close();
+            answersTextFileReader.Close();
+            //Console.WriteLine(questions[0]);
+            //Console.WriteLine(answers[0]);
+            // Arrays are now populated with text content from external text files.
+
             switch (userchoice)
             {
                 case 1:
