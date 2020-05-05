2fe2eb39c94c683b265c3b61fa52a9694ee7957d
leggant <61127354+leggant@users.noreply.github.com>
2020-05-05 12:08:37 +1200

    Tidied up intro text

--- TeamAProfessionalPracticeGroupProject/TeamAProfessionalPracticeGroupProject/PPGroupProject.cs
+++ TeamAProfessionalPracticeGroupProject/TeamAProfessionalPracticeGroupProject/PPGroupProject.cs
@@ -8,7 +8,7 @@ namespace TeamAProfessionalPracticeGroupProject
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("################################################################");
-            Console.Write("###########Welcome, This Program is Here To Help You!\nCovid-19 Lockdown has been tough, so we are here to check in.");
+            Console.WriteLine("###########Welcome, This Program is Here To Help You!###########\nCovid-19 Lockdown has been tough, so we are here to check in.");
             Console.WriteLine("################################################################");
             Console.ForegroundColor = ConsoleColor.White;
             Console.ReadLine();
