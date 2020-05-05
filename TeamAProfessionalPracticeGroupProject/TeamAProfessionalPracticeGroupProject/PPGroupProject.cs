1fe385081a2053a9604ce0617efb37f0468a7515
leggant <61127354+leggant@users.noreply.github.com>
2020-05-05 12:00:12 +1200

    Starting into text

--- TeamAProfessionalPracticeGroupProject/TeamAProfessionalPracticeGroupProject/PPGroupProject.cs
+++ TeamAProfessionalPracticeGroupProject/TeamAProfessionalPracticeGroupProject/PPGroupProject.cs
@@ -6,6 +6,11 @@ namespace TeamAProfessionalPracticeGroupProject
     {
         static void Main()
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("################################################################");
+            Console.Write("###########Welcome, This Program is Here To Help You!\nCovid-19 Lockdown has been tough, so we are here to check in.");
+            Console.WriteLine("################################################################");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.ReadLine();
         }
     }
