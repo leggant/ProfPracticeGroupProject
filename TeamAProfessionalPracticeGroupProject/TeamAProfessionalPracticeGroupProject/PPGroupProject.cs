65873647f8c470fe0e2d0c6a06e74ea0980f700b
leggant <61127354+leggant@users.noreply.github.com>
2020-05-18 17:35:17 +1200

    Added Randomly Generated Question output

--- TeamAProfessionalPracticeGroupProject/TeamAProfessionalPracticeGroupProject/PPGroupProject.cs
+++ TeamAProfessionalPracticeGroupProject/TeamAProfessionalPracticeGroupProject/PPGroupProject.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO; // For Importing Text Strings From Text Files
-using System.Linq;
 using TeamAProfessionalPracticeGroupProject;
 
 namespace CatchUp19
@@ -18,7 +17,7 @@ namespace CatchUp19
             string[] negativeAnswers = new string[7];
             string[] affirmations = new string[6];
             Random answerGenerator = new Random();
-            int affirmationslen, randAffirmations;
+            int affirmationslen, randAffirmations, questionSelector;
             string nameInput, temp = "";
             string hashlines = "################################################################";
             string welcomeText = "###########Welcome, Catch-Up-19 is Here To Help You!###########\nCovid-19 Lockdown has been tough, so we are here to check in.";
@@ -104,82 +103,32 @@ namespace CatchUp19
             //using switch to excute diffrent commands based on the user input
             switch (userChoice)
             {
-                //Negative Mood 
+                //Negative Mood
                 case 1:
                 case 2:
                 case 3:
                 case 4:
-                    for (int i = 0; i < negativeQuestions.Length; i++)
-                    {
-                        Console.WriteLine(negativeQuestions[i]);
-                    }
-                    //iterate 2 arrays in a single foreach loop
-                    //var QuestionsandAnswers = questions[1..8].Zip(answers[1..8], (q, a) => new { question = q, answer = a });
-                    //foreach (string question in negativeQuestions)
-                    //{
-                    //    Console.WriteLine(question);
-
-                    //    Console.Write("Please Enter..(yes or no)");
-                    //    temp = Console.ReadLine();
-                    //    if (temp == "yes" || temp == "YES")
-                    //    {
-                    //        Console.WriteLine();
-                    //        Console.WriteLine(question);
-                    //        //ask the user if they want to continue or to stop the program
-                    //        Console.WriteLine();
-                    //        Console.Write($"Need more help {nameInput}..(yes or no)");
-                    //        temp = Console.ReadLine();
-                    //        if (temp != "yes")
-                    //        {
-                    //            break;
-                    //        }
-                    //    }
-                    //}
-                    //// a message for the user before exiting the program
-                    //Console.WriteLine("\nThank you and stay safe");
+                     questionSelector = answerGenerator.Next(negativeQuestions.Length);
+                        Console.Write($"{negativeQuestions[questionSelector]}: ");
                     break;
                 //Meh Mood
                 case 5:
                 case 6:
-                    //iterate 2 arrays in a single foreach loop
-                    //var questionsAndAnswers2 = questions[11..15].Zip(answers[17..21], (q, a) => new { question = q, answer = a });
-                    for (int i = 0; i < mehQuestions.Length; i++)
-                    {
-                        Console.WriteLine(mehQuestions[i]);
-                    }
-                    //foreach (string qa in mehQuestions)
-                    //{
-                    //    Console.WriteLine(qa);
-                    //    Console.WriteLine("Enter your answer");
-                    //    temp = Console.ReadLine();
-                    //    if (temp == "yes")
-                    //    {
-                    //        Console.WriteLine(qa);
-                    //        //ask the user if they want to continue or to stop the program
-                    //        Console.WriteLine("\nFeel better ? or Need more help");
-                    //        temp = Console.ReadLine();
-                    //        if (temp != "yes")
-                    //        {
-                    //            break;
-                    //        }
-                    //    }
-                    //}
-                    //// a message for the user before exiting the program
-                    //Console.WriteLine("\nThank you and stay safe");
+                    questionSelector = answerGenerator.Next(mehQuestions.Length);
+                    Console.Write($"{mehQuestions[questionSelector]} : ");
                     break;
                 // Positive Mood
                 case 7:
                 case 8:
                 case 9:
                 case 10:
-                    for (int i = 0; i < positiveQuestions.Length; i++)
-                    {
-                        Console.WriteLine(positiveQuestions[i]);
-                    }
+                    questionSelector = answerGenerator.Next(positiveQuestions.Length);
+                    Console.Write($"{positiveQuestions[questionSelector]} : ");
+                    //read in answer? or follow a set script?
                     affirmationslen = affirmations.Length;
                     randAffirmations = answerGenerator.Next(1, affirmationslen);
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write($"\n\t{affirmations[randAffirmations]}");
+                    Console.Write($"\n{affirmations[randAffirmations]}");
                     Console.ForegroundColor = ConsoleColor.White;
                     break;
             }
